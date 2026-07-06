bfadccfd6e5af20735b3de3c69d59ed0d0b510c8
Jonas <K104Jonas@gmail.com>
2026-07-06 11:35:15 +0200

    Remove unused DOMAIN import and streamline async_unload_entry logic

--- custom_components/aldi_talk/__init__.py
+++ custom_components/aldi_talk/__init__.py
@@ -8,7 +8,6 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN
 from .coordinator import AldiTalkCoordinator
 
 _LOGGER = logging.getLogger(__name__)
@@ -18,8 +17,6 @@ PLATFORMS = [Platform.SENSOR]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up ALdi Talk from a config entry."""
-
-    hass.data.setdefault(DOMAIN, {})
 
     coordinator = AldiTalkCoordinator(hass, entry)
 
@@ -36,10 +33,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unload_ok
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
 
 async def _async_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
